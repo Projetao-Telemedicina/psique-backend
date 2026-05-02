abbf0e51688b5755b587fa6c022216c1e101ce94
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2026-05-02 17:12:39 -0300

    refactor: update DTOs to use PartialType

--- src/patients/dto/update-patient-profile.dto.ts
+++ src/patients/dto/update-patient-profile.dto.ts
@@ -1,19 +1,6 @@
 import { IsOptional, IsString, IsDateString, IsBoolean } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePatientProfileDto } from './create-patient-profile.dto';
+import { PickType } from '@nestjs/swagger';
 
-export class UpdatePatientProfileDto {
-  @IsOptional()
-  @IsDateString()
-  birthDate?: string;
-
-  @IsOptional()
-  @IsString()
-  emergencyContactName?: string;
-
-  @IsOptional()
-  @IsString()
-  emergencyContactPhone?: string;
-
-  @IsOptional()
-  @IsBoolean()
-  shareDiaryWithProfessionals?: boolean;
-}
+export class UpdatePatientProfileDto extends PartialType(PickType(CreatePatientProfileDto, ['emergencyContactName', 'emergencyContactPhone', 'shareDiaryWithProfessionals'] as const)) {}
