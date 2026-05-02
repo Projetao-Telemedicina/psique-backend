5d5330961a302efed6a10622c7e3dd6bb3c3c8e4
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2026-05-02 17:57:43 -0300

    fix: use correct import path

--- src/patients/patients.service.ts
+++ src/patients/patients.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaService } from '@/prisma/index';
 import { UpdatePatientProfileDto } from './dto/update-patient-profile.dto';
 
 @Injectable()
