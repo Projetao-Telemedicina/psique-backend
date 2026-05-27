9459e8820eb47a4071c2d78ae34cd69653fcc02a
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2026-05-26 21:48:48 -0300

    test: add e2e tests for matching

--- src/matching/dto/index.ts
+++ src/matching/dto/index.ts
@@ -1,3 +1,5 @@
 export { CreatePatientQuestionnaireDto } from './patient-questionnaire.dto';
+
 export { CreateProfessionalQuestionnaireDto } from './professional-questionnaire.dto';
+
 export { MatchResponseDto, MatchRecommendationDto } from './match-response.dto';
--- src/matching/index.ts
+++ src/matching/index.ts
@@ -1,3 +1,5 @@
 export { MatchingModule } from './matching.module';
+
 export { MatchingService } from './matching.service';
+
 export { MatchingController } from './matching.controller';
